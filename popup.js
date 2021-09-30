d550d7fd74545cc8cc8e902153c6d5bf769b2f42
Neonetix <antoine@msn.com>
2021-09-30 13:20:33 +0200

    POC

--- popup.js
+++ popup.js
@@ -1,12 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let checkPageButton = document.getElementById('checkPage');
-    let test = document.getElementById('test');
-    checkPageButton.addEventListener('click', function() {
-        chrome.tabs.getSelected(null, function(tab) {
-            let d = document;
-            let url = tab.url
-            let text = d.createTextNode(url)
-            test.append(text)
-        });
-    }, false);
-}, false);
+document.addEventListener('DOMContentLoaded', function () {
+    let form = document.getElementById("formReport");
+    form.addEventListener('submit', function () {
+        alert('Merci !')
+    })
+})
